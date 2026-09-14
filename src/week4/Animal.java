ea98c59bbcab4983704682e1f4cae0e2b5c93aee
Joe Oakes <joe.oakes@gmail.com>
2026-09-14 09:04:29 -0400

    Commit all

---
+++ src/week4/Animal.java
@@ -0,0 +1,8 @@
+package week4;
+
+class Animal {
+    protected int legs;
+    public void eat() {
+        System.out.println("Animal eats");
+    }
+}
---
+++ src/week4/Dog9.java
@@ -0,0 +1,7 @@
+package week4;
+
+class Dog9 extends Animal {
+    Dog9() {
+        legs = 4;
+    }
+}
