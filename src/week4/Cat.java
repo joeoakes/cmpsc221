aa953427073888bd9c0b257a83ac75706f2d34e0
Joe Oakes <joe.oakes@gmail.com>
2026-09-16 16:21:38 -0400

    Commit all

--- src/week4/Cat.java
+++ src/week4/Cat.java
@@ -1,6 +1,12 @@
 package week4;
 
 class Cat extends Animal {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        cat.eat();
+        cat.makeSound();
+        cat.legs = 3;
+    }
     public void makeSound() {
         System.out.println("Meow");
     }
