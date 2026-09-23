56d98a5d12b920a56aee8652222358d3e5287bf0
Joe Oakes <joe.oakes@gmail.com>
2026-09-23 13:19:31 -0400

    Commit all

---
+++ src/Week4_5/Casting.java
@@ -0,0 +1,14 @@
+package Week4_5;
+
+public class Casting {
+    public static void main(String[] args) {
+        int a = (int) 3.14;
+        System.out.println(a); //Outputs 3
+
+        double aa = 42.571;
+        int b = (int) aa;
+        System.out.println(b); //Outputs 42
+
+    }
+
+}
